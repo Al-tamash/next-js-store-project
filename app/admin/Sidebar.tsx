2d1da80b3395f12cd7adb103cf733b0e5fb10c6b
Md Altamash <mdaltamashkalim24@gmail.com>
2025-10-06 12:55:30 +0530

    admin sidebar prop key missing solve

--- app/admin/Sidebar.tsx
+++ app/admin/Sidebar.tsx
@@ -1,21 +1,21 @@
 'use client'
-import { Button } from '@/components/ui/button'
-import { adminLinks, links } from '@/utils/links'
+import { adminLinks } from '@/utils/links'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import React from 'react'
+import { Button } from '@/components/ui/button'
 
 function Sidebar() {
   const pathname = usePathname()
+
   return (
     <aside>
       {adminLinks.map((link) => {
-        const isActive = pathname === link.href
-        const variant = isActive ? 'default' : 'ghost'
+        const isActivePage = pathname === link.href
+        const variant = isActivePage ? 'default' : 'ghost'
         return (
           <Button
             asChild
-            className='w-full mb-2 justify-start font-normal capitalize'
+            className='w-full mb-2 capitalize font-normal justify-start'
             variant={variant}
           >
             <Link key={link.href} href={link.href}>
@@ -27,5 +27,4 @@ function Sidebar() {
     </aside>
   )
 }
-
 export default Sidebar
